109a7d0a70211354878dd30d0c8557226f249e87
luis25550586 <l25550586@chihuahua2.tecnm.mx>
2025-08-27 20:25:02 -0600

    Add files via upload

---
+++ EVA1_5_VARIABLES/src/eva1_5_variables/EVA1_5_VARIABLES.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_5_variables;
+
+/**
+ *
+ * @author Luism
+ */
+public class EVA1_5_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //DECARACION DE VARIABLES
+        //crear las variables para que se puedan usar
+        //tipo de dato intificador
+        String nombre;
+        byte edad;
+        //Inicializacion
+        //USAMO DEL OPERADOR ----- OPERADOR DE ASIGNACION
+        //PON "LUIS MARQUEZ" DENTRO DE LA VARIABLE DE NOMBRE
+        nombre = "Luis Marquez";
+        edad = 17;
+        //Uso de variables
+        System.out.println(nombre);
+        System.out.println(edad);
+    }
+    
+}
